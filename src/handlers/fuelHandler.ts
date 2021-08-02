2f7f8c0fc8f2262c15715f6ab09bb72d8d11189c
Alfanhui <program.foo@gmail.com>
2021-08-02 20:03:05 +0100

    Remove unused console.log

--- src/handlers/fuelHandler.ts
+++ src/handlers/fuelHandler.ts
@@ -33,7 +33,6 @@ export async function syncFuel(provider: MongoProvider, esi: ESI, discordNotifie
         console.error(e)
         return null;
     }
-    console.log("fuelWar complete")
 }
 
 async function compileEmbedMessage(esi: ESI, corperation: Corperation, token: Token, corpStructure: CorpStructure, currentFuelStatus: FuelNotify): Promise<MessageEmbed> {
